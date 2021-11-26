a371f1f12350626ed30c0918be450374a0aca864
Azmiroff <Mirsoliev9099@gmail.com>
2021-11-26 05:01:37 +0500

    "add js file"

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var numberList = [2,3,5,6,7,8,9,10,12,13];
 function isSumHasInArray(arr,num){
     for(var i=0; i<arr.length; i++){
         for(var j=0; j<arr.length; j++){
-            if(arr[i]+arr[j]==num){
+            if(arr[i]+arr[j]==num){~
                 return true;
             }
         }
